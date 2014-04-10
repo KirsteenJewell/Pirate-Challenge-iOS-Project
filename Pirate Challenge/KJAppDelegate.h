967a63bb66087f8381141ad67b47d7754ef53d5f
Kirsteen Jewell <kirsteenjewell@kirsteens-imac.limitless.co.uk>
2014-04-10 15:39:07 +0100

    Initial Commit

---
+++ Pirate Challenge/KJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KJAppDelegate.h
+//  Pirate Challenge
+//
+//  Created by Kirsteen Jewell on 10/04/2014.
+//  Copyright (c) 2014 limitless. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Pirate Challenge/KJViewController.h
@@ -0,0 +1,13 @@
+//
+//  KJViewController.h
+//  Pirate Challenge
+//
+//  Created by Kirsteen Jewell on 10/04/2014.
+//  Copyright (c) 2014 limitless. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KJViewController : UIViewController
+
+@end
